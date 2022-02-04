4b7d1d58b204ff142ef4aceda20ce378f98acc84
Vrushali <vkharde@deloitte.com>
2022-02-04 11:17:08 +0530

    adding initial commi

---
+++ lamda-funct/hello.js
@@ -0,0 +1,18 @@
+// Lambda function code
+
+module.exports.handler = async (event) => {
+  console.log('Event: ', event);
+  let responseMessage = 'Welcome to our demo API, here are the details of your request!';
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: responseMessage,
+      "username":"xyz", 
+      "password":"xyz"
+    }),
+  }
+}
---
+++ main.tf
@@ -0,0 +1,63 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.48.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.2.0"
+    }
+  }
+
+  required_version = "~> 1.0"
+}
+
+provider "aws" {
+  region = var.aws_region
+  access_key="AKIA5T4QMEF5LZPRXXM7"
+  secret_key="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"
+}
+
+
+resource "aws_lambda_function" "hello_world" {
+  function_name = "terraformlamdapoc"
+
+
+  runtime = "nodejs12.x"
+  handler = "hello.handler"
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name = "/aws/lambda/${aws_lambda_function.hello_world.function_name}"
+
+  retention_in_days = 30
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
