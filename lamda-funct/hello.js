a4c14578b1089c62567085cac56f0da1f7c84186
khardev <76969963+khardev@users.noreply.github.com>
2022-02-05 15:40:28 +0530

    Update hello.js

--- lamda-funct/hello.js
+++ lamda-funct/hello.js
@@ -1,25 +1,24 @@
 module.exports.handler = async (event) => {
   console.log('Event: ', event)
   let responseMessage = 'Welcome to our demo API, here are the details of your request';
- let body;
- if (event.body) {
-       body = JSON.parse(event.body)
-   console.log(body);
-   }
- console.log("console logs");
- console.log(body.username);
- console.log(body.password);
- let username = body.username;
- let password = body.password;
-  return {
-   statusCode: 200,
-   headers: {
-     'Content-Type': 'application/json',
-   },
-   body: JSON.stringify({
-     message: responseMessage,
-     "username":username,
-     "password":password
-   }),
+
+ let username;
+ let password;
+ 
+ if (event.queryStringParameters && event.queryStringParameters['username'] && event.queryStringParameters['password']) {
+  username = event.queryStringParameters['username']
+  password = event.queryStringParameters['password']
  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      message: responseMessage,
+     "username": username,
+     "password": password
+    }),
+  }
 }
