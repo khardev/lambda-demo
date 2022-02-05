fe7b659fa3d759be16c6cb59706491b165f1b580
khardev <76969963+khardev@users.noreply.github.com>
2022-02-05 13:52:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_apigatewayv2_stage" "lambda" {
 resource "aws_apigatewayv2_integration" "hello_world" {
   api_id = aws_apigatewayv2_api.lambda.id
 
-  integration_uri    = aws_lambda_function.hello_world.invoke_arn
+  integration_uri    = aws_lambda_function.lamda_demo.invoke_arn
   integration_type   = "AWS_PROXY"
   integration_method = "POST"
 }
@@ -150,7 +150,7 @@ resource "aws_cloudwatch_log_group" "api_gw" {
 resource "aws_lambda_permission" "api_gw" {
   statement_id  = "AllowExecutionFromAPIGateway"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello_world.function_name
+  function_name = aws_lambda_function.lamda_demo.function_name
   principal     = "apigateway.amazonaws.com"
 
   source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
